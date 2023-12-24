f3aa5e754e549be163c2a658c2e6e4726734621b
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-12-24 09:07:27 +0000

    modified:   src/compile.c

--- src/compile.c
+++ src/compile.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int compile(char * compile_cmd){
     char * buffer = malloc(strlen(compile_cmd)+37);
-    sprintf(buffer, "%s -Xclang -ast-dump=json > ast.json",  compile_json);
+    sprintf(buffer, "%s -Xclang -ast-dump=json > ast.json",  compile_cmd);
     return system(buffer);
 }
