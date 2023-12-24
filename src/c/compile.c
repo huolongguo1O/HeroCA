d0198fa05cd40b262b98cc4f795e6294d1dfe30b
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-12-24 10:55:38 +0000

    renamed:    src/ast_analyze.c -> src/c/ast_analyze.c 	renamed:    src/compile.c -> src/c/compile.c 	modified:   src/main.c

--- src/c/compile.c
+++ src/c/compile.c
@@ -2,8 +2,8 @@
 #include <stdlib.h>
 #include <string.h>
 
-int compile(char * compile_cmd){
+int compile_c(char * compile_cmd){
     char * buffer = malloc(strlen(compile_cmd)+37);
-    sprintf(buffer, "%s -Xclang -ast-dump=json > ast.json",  compile_cmd);
+    sprintf(buffer, "%s -Xclang -ast-dump=json > /tmp/ast.json",  compile_cmd);
     return system(buffer);
 }
--- src/main.c
+++ src/main.c
@@ -1,6 +1,7 @@
 #include<stdio.h>
 #include "args.h"
-char * _global_compile_cmd;
+char * _global_compile_cmd = 0;
+char * honggfuzz_bin;
 int main(char *argv[], int argc){
     int arg = parse_arg(argv, argc);
     if(ISSET(arg, ARG_HELP)||ISSET(arg, ARG_VERSION)){
@@ -9,6 +10,14 @@ int main(char *argv[], int argc){
     }
     if(ISSET(arg, ARG_RUN_HONGGFUZZ)){
         //TODO
-        
+        if(fork()){
+
+        }
     }
+    if(!_global_compile_cmd){
+        //TODO: Auto compile
+    }
+    compile_c(_global_compile_cmd);
+    analyze_c();
+    return 0;
 }
