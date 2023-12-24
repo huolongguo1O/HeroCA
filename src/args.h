43215a74704b2ea7e8842de8d9ce2286060bb516
huolongguo1O <121071167+huolongguo1O@users.noreply.github.com>
2023-12-24 10:48:12 +0000

    bugfix

--- src/args.h
+++ src/args.h
@@ -1,5 +1,5 @@
-#define ARG_HELP 1;
-#define ARG_VERSION 2;
-#define ARG_RUN_HONGGFUZZ 4;
+#define ARG_HELP 1
+#define ARG_VERSION 2
+#define ARG_RUN_HONGGFUZZ 4
 
 #define ISSET(var, bit) (((var) & (~bit)) != var)
